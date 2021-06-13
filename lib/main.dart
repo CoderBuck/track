2c6086c6539acc72a4dee8a13f263d89a7fca548
codebuck <coderbuck@163.com>
2021-06-13 23:52:23 +0800

    修改字体和颜色

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:track/route/app_pages.dart';
 
 void main() {
@@ -16,11 +17,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Track',
-      theme: ThemeData(
-        primaryColor: Colors.grey.shade900,
-        primaryColorDark: Colors.grey.shade900,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey.shade900,
+        textTheme: GoogleFonts.jetBrainsMonoTextTheme(
+          Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          color: Colors.grey.shade900,
+          textTheme: GoogleFonts.jetBrainsMonoTextTheme(
+            Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white
+            ),
+          ),
+          elevation: 0,
+          centerTitle: false,
+        )
       ),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:track/page/home/home_model.dart';
 import 'package:track/util/date_time_util.dart';
 
@@ -8,11 +9,19 @@ class HomePage extends GetWidget<HomeModel> {
   Widget build(BuildContext context) {
     var daysInYear = DateTimeUtil.daysInYear();
     var daysOneYear = DateTimeUtil.daysOneYear();
+
     return Scaffold(
-      backgroundColor: Colors.grey.shade900,
       appBar: AppBar(
-        elevation: 0,
-        title: Text('Track  $daysInYear/$daysOneYear'),
+        title: Text.rich(TextSpan(children: [
+          TextSpan(
+            text: 'Track  ',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          TextSpan(
+            text: '$daysInYear/$daysOneYear',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ])),
         actions: [
           Container(
             padding: const EdgeInsets.all(16.0),
@@ -28,17 +37,20 @@ class HomePage extends GetWidget<HomeModel> {
       ),
       body: GridView.builder(
         itemCount: DateTimeUtil.daysOneYear(),
-        padding: const EdgeInsets.all(6),
+        padding: const EdgeInsets.all(16),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 15,
         ),
+
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(2),
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(4),
-                color: index + 1 < daysInYear ? Colors.grey : Colors.green,
+                color: index + 1 < daysInYear ? Colors.grey.shade600
+                    : index + 1 == daysInYear ? Colors.green
+                    : Colors.grey.shade500
               ),
             ),
           );
